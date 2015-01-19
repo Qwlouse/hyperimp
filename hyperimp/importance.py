ea2f053c3d2cf79e5c229e9004348b9223898d0c
Klaus Greff <klaus@idsia.ch>
2015-01-19 17:36:18 +0100

    implemented multi-interaction support

--- hyperimp/importance.py
+++ hyperimp/importance.py
@@ -2,29 +2,49 @@
 # coding=utf-8
 from __future__ import division, print_function, unicode_literals
 import numpy as np
+from itertools import combinations, product
+
 from hyperimp.marg_func import MarginalizingFunction, \
     MarginalizeOverDimsFunction
 from hyperimp.utils import divide_global_space
 
 
+def get_fu(conf_space, partitions, low_coords, us):
+    func_u = MarginalizeOverDimsFunction(conf_space, partitions, us)
+    f_u = func_u.get_marginal(low_coords)
+
+    for l in range(len(us)):
+        for idxs, w in zip(combinations(range(len(us)), l),
+                           combinations(us, l)):
+            f_u -= get_fu(conf_space, partitions, low_coords[list(idxs), :], w)
+
+    return f_u
+
+
 def quantify_importance(configuration_space, tree, K=1):
     func = MarginalizingFunction(configuration_space, tree)
     global_divisions = divide_global_space(configuration_space, tree)
-    f_avg = func.get_marginal()
     f_var = func.get_marginal_var()
-    F = []
-    #for k in range(K):
-    for u in range(len(configuration_space)):
-        u_range = (configuration_space[u][1] - configuration_space[u][0])
-        if u_range == 0:
-            continue
-        func_u = MarginalizeOverDimsFunction(configuration_space,
-                                             func.partitions, [u])
-        a_u = func_u.get_marginal([global_divisions[u][:-1]])
-        f_u = a_u - f_avg  # this is only true for K=1
-        #                    (it needs dynamic programming later on)
-        lows = np.array(global_divisions[u][:-1])
-        highs = np.array(global_divisions[u][1:])
-        var_u = np.sum(f_u ** 2 * (highs - lows) / u_range)
-        F.append(var_u / f_var)
+    F = {}
+    for k in range(1, K + 1):
+        for us in combinations(range(len(configuration_space)), k):
+            u_range = np.product([(configuration_space[u][1] -
+                                   configuration_space[u][0]) for u in us])
+            if u_range == 0:
+                continue
+
+            # get the "lower left corners" of all the regions
+            low_coords = np.array(list(product(*[global_divisions[u][:-1]
+                                                 for u in us]))).T
+            # get the "upper right corners" of all the regions
+            high_coords = np.array(list(product(*[global_divisions[u][1:]
+                                                  for u in us]))).T
+
+            # calculate the sizes of all the regions
+            sizes = np.product(high_coords - low_coords, axis=0)
+
+            f_u = get_fu(configuration_space, func.partitions, low_coords, us)
+
+            var_u = np.sum(f_u ** 2 * sizes / u_range)
+            F[us] = var_u / f_var
     return F
