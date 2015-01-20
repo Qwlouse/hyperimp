a449791176150fe59eab4507db901fe89fa2e5e0
Klaus Greff <klaus@idsia.ch>
2015-01-20 19:06:23 +0100

    updated analyse for use with higher order interaction

--- hyperimp/analysis.py
+++ hyperimp/analysis.py
@@ -5,6 +5,7 @@ import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from hyperimp.utils import get_configuration_space, get_partitions
 from hyperimp.importance import quantify_importance
+from hyperimp.marg_func import MarginalizeOverDimsFunction
 
 
 def get_forest(parameters, performance, n_trees=100):
@@ -14,16 +15,19 @@ def get_forest(parameters, performance, n_trees=100):
     return clf
 
 
-def analyse(params, perf, param_names=None, n_trees=100):
+def analyse(params, perf, K=1, param_names=None, n_trees=100):
     # train predictor
     clf = get_forest(params, perf, n_trees)
 
     configuration_space = get_configuration_space(params)
     var_fracs = []
     partitionings = []
+    key_combinations = []
     for est in clf.estimators_:
         tree = est.tree_
-        var_fracs.append(quantify_importance(configuration_space, tree))
+        fu = quantify_importance(configuration_space, tree, K=K)
+        key_combinations = sorted(fu.keys())
+        var_fracs.append([fu[k] for k in key_combinations])
         partitionings.append(get_partitions(tree, configuration_space))
 
     result = {
@@ -36,8 +40,10 @@ def analyse(params, perf, param_names=None, n_trees=100):
     if param_names is None:
         param_names = ['X%d' % i for i in range(len(mean_vars))]
 
-    for name, mv in zip(param_names, mean_vars):
-        print(name, "= %.2f%%" % (mv * 100))
+    main_vars = sorted(zip(key_combinations, mean_vars), key=lambda x: -x[1])
+    for i, (keys, mv) in enumerate(main_vars):
+        name = " × ".join([param_names[j] for j in keys])
+        print("%d)" % i, name, "= %.2f%%" % (mv * 100))
         result['marginals'][name] = mv
 
     for u, name in enumerate(param_names):
@@ -50,3 +56,4 @@ def analyse(params, perf, param_names=None, n_trees=100):
         result['avg_stds'][name] = X, np.mean(margs, 0), np.std(margs, 0)
 
     return result
+
